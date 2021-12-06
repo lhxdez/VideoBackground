31d8e085b2ca4afba2d338404c1d09879327b11b
Lucas Gomes <lucas.gomes@startse.com>
2021-12-06 16:36:54 -0300

    feat: carrossel

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:video_player/video_player.dart';
 
 void main() {
@@ -14,6 +16,17 @@ class VideoTest extends StatefulWidget {
 
 class _VideoTestState extends State<VideoTest> {
   late VideoPlayerController _videoPlayerController;
+  final contentText = [
+    "Fique por dentro das principais inovações que transformam sua carreira e seu negócio",
+    "Descubra diariamente novos temas e conteúdos que realmente importam para você",
+    "Aprenda as estratégias de sucesso com as empresas mais inovadoras do mundo"
+  ];
+  final titleText = [
+    "Conhecimento do agora",
+    "Experiência Personalizada",
+    "Imersões Internacionais"
+  ];
+  final String logo = 'lib/assets/startSe.svg';
 
   @override
   void initState() {
@@ -42,25 +55,58 @@ class _VideoTestState extends State<VideoTest> {
         child: Scaffold(
           body: Stack(
             children: <Widget>[
-              SizedBox.expand(
-                child: FittedBox(
-                  fit: BoxFit.cover,
-                  child: SizedBox(
-                    height: _videoPlayerController.value.size.height,
-                    width: _videoPlayerController.value.size.width,
-                    child: VideoPlayer(_videoPlayerController),
-                  ),
-                ),
-              ),
-              Container(
-                margin: const EdgeInsets.only(left: 20, bottom: 20),
-                child: const Align(
-                    alignment: Alignment.bottomLeft,
-                    child: Text(
-                      'Text',
-                      style: TextStyle(color: Colors.white, fontSize: 20),
-                    )),
-              ),
+              // SizedBox.expand(
+              //   child: FittedBox(
+              //     fit: BoxFit.cover,
+              //     child: SizedBox(
+              //       height: _videoPlayerController.value.size.height,
+              //       width: _videoPlayerController.value.size.width,
+              //       child: VideoPlayer(_videoPlayerController),
+              //     ),
+              //   ),
+              // ),
+              Align(
+                  alignment: Alignment.bottomLeft,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.max,
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      SvgPicture.asset(
+                        logo,
+                        color: Colors.black,
+                        semanticsLabel: 'Startse Logo',
+                        fit: BoxFit.contain,
+                        width: 62,
+                      ),
+                      Container(
+                        width: 200,
+                        child: CarouselSlider.builder(
+                          options: CarouselOptions(
+                            height: 50,
+                            autoPlay: true,
+                          ),
+                          itemCount: contentText.length,
+                          itemBuilder: (context, itemIndex, realIndex) {
+                            return Column(
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Text(titleText[itemIndex],
+                                      textAlign: TextAlign.left,
+                                      style:
+                                          const TextStyle(color: Colors.red)),
+                                ),
+                                Text(contentText[itemIndex],
+                                    style: const TextStyle(
+                                        color: Colors.red, fontSize: 6)),
+                              ],
+                            );
+                          },
+                        ),
+                      ),
+                    ],
+                  )),
             ],
           ),
         ),
